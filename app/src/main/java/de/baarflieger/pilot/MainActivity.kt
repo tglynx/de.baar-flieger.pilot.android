ba293dbd5ddd631c1654f26581c5d9d6ce9c59ad
Michael Sommer <git@neuronetix.de>
2023-05-01 09:54:24 +0200

    Bugfix / not Starting on older Android Versions (Angle Attribute in Gradient must be a multiple of 45)

--- app/src/main/java/de/baarflieger/pilot/MainActivity.kt
+++ app/src/main/java/de/baarflieger/pilot/MainActivity.kt
@@ -67,7 +67,7 @@ class MainActivity : ComponentActivity() {
             //WebViewPage("file:///android_asset/shop.html") //OFFLINE
         }
     }
-}
+    }
 
 @OptIn(ExperimentalComposeUiApi::class)
 @SuppressLint("SetJavaScriptEnabled")
@@ -241,26 +241,26 @@ fun WebViewPage(url: String){
 }
 
 
-fun isOnline(context: Context): Boolean {
-    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-    // For 29 api or above
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork) ?: return false
-        return when {
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ->    true
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ->   true
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ->   true
-            else ->     false
+        // For 29 api or above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork) ?: return false
+            return when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ->    true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ->   true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ->   true
+                else ->     false
+            }
         }
-    }
-    // For below 29 api
-    else {
-        @Suppress("DEPRECATION")
-        if (connectivityManager.activeNetworkInfo != null && connectivityManager.activeNetworkInfo!!.isConnectedOrConnecting) {
-            return true
+        // For below 29 api
+        else {
+            @Suppress("DEPRECATION")
+            if (connectivityManager.activeNetworkInfo != null && connectivityManager.activeNetworkInfo!!.isConnectedOrConnecting) {
+                return true
+            }
         }
-    }
-    return false
+        return false
 
-}
+    }
